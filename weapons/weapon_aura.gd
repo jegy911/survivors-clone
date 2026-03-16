0fa12851a4a2815625d6fae5aa796f4e52595cc9
jegy911 <151401071+jegy911@users.noreply.github.com>
2026-03-16 04:37:58 +0300

    Update weapon_aura.gd

--- weapons/weapon_aura.gd
+++ weapons/weapon_aura.gd
@@ -3,6 +3,8 @@ extends WeaponBase
 
 var radius = 80.0
 var slow_factor = 0.5
+var hit_cooldowns = {}
+const HIT_INTERVAL = 0.8
 
 func _ready():
 	super._ready()
@@ -15,14 +17,22 @@ func _ready():
 func attack():
 	var effective_radius = radius * player.get_area_multiplier()
 	var enemies = get_tree().get_nodes_in_group("enemies")
+	# Hit cooldown'ları temizle
+	for key in hit_cooldowns.keys():
+		hit_cooldowns[key] -= get_effective_cooldown()
+		if hit_cooldowns[key] <= 0:
+			hit_cooldowns.erase(key)
 	for enemy in enemies:
+		var enemy_id = enemy.get_instance_id()
+		if hit_cooldowns.has(enemy_id):
+			continue
 		if player.global_position.distance_to(enemy.global_position) <= effective_radius:
 			var final_damage = player.get_total_damage(damage)
 			enemy.take_damage(final_damage)
 			EventBus.on_damage_dealt.emit(player, enemy, final_damage)
-			
 			if enemy.has_method("apply_slow"):
 				enemy.apply_slow(slow_factor, 1.5)
+			hit_cooldowns[enemy_id] = HIT_INTERVAL
 			
 
 func on_upgrade():
