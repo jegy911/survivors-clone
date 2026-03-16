0759654c155418e7dd5c13476bfaca028e167c43
jegy911 <151401071+jegy911@users.noreply.github.com>
2026-03-16 03:21:04 +0300

    Update audio_manager.gd

--- core/audio_manager.gd
+++ core/audio_manager.gd
@@ -69,6 +69,7 @@ func apply_volume_settings():
 		AudioServer.set_bus_send(music_bus, "Master")
 
 func play_shoot():
+	print("shoot bus: ", shoot_player.bus)
 	shoot_player.play()
 func play_hit():
 	hit_player.play()
